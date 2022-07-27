cd87966b63a03ac8f9c73167530532dd25fe6355
Martasantenbo <martasan.donuts@gmail.com>
2022-07-27 16:26:17 +0200

    add(Main.vue) added svg and rectangle

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="inline-block pr-175">
-    <h1 class="font-text text-46px pt-20">Hello, my name is</h1>
-    <h1 class="font-text text-72px py-3">MARTIN ČERNOCH</h1>
-    <h1 class="font-text text-46px">FrontEnd developer</h1>
+  <div class="svgOne">
+    <div class="inline-block pr-175">
+      <h1 class="font-text text-46px pt-20">Hello, my name is</h1>
+      <h1 class="font-text text-72px py-3">MARTIN ČERNOCH</h1>
+      <h1 class="font-text text-46px">FrontEnd developer</h1>
+    </div>
+    <div class="rectangle w-178 h-99 text-36px font-text text-left">
+      <p class="pt-15 pl-4">const data = [
+      <p class="pl-8">{</p>
+      <p class="pl-11">userName: ‘Martin Černoch’,
+        userBDay: ‘27th December 2004’,
+        userLikes: ‘Computer-oriented stuff’</p>
+      <p class="pl-8">{</p>
+      <p class="pl-4">]</p>
+      </p>
+    </div>
   </div>
-  <div class="svgOne"></div>
 </template>
 <style scoped lang="scss">
 .svgOne {
   background-image: url("../assets/images/svg-one.svg");
   background-repeat: no-repeat;
   background-position: right;
+}
+
+.rectangle {
+  border-width: 8px;
+  border-radius: 1.5rem;
+  border-color: #42B883;
+  background-color: #FFFFFF;
 }
 </style>
